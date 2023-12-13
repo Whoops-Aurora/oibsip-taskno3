81de5e51cb93dbf0c1d85199a6f0b81eea4a350e
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-13 21:32:22 +0530

    "Ready!"

--- script.js
+++ script.js
@@ -2,25 +2,50 @@ function convertTemperature() {
     const temperatureInput = document.getElementById('temperatureInput').value;
     const unitSelect = document.getElementById('unitSelect').value;
     const resultElement = document.getElementById('result');
+    const emojiElement = document.getElementById('emoji');
+    const body = document.body;
     let result = '';
   
     if (!temperatureInput || isNaN(temperatureInput)) {
       result = 'Please enter a valid temperature!';
     } else {
       const temperature = parseFloat(temperatureInput);
+      let convertedTemperature = 0;
+  
       switch (unitSelect) {
         case 'celsius':
-          result = `${temperature}°C is ${((temperature * 9) / 5 + 32).toFixed(2)}°F`;
+          convertedTemperature = ((temperature * 9) / 5) + 32;
+          result = `${temperature}°C is ${convertedTemperature.toFixed(2)}°F`;
           break;
         case 'fahrenheit':
-          result = `${temperature}°F is ${(((temperature - 32) * 5) / 9).toFixed(2)}°C`;
+          convertedTemperature = (((temperature - 32) * 5) / 9);
+          result = `${temperature}°F is ${convertedTemperature.toFixed(2)}°C`;
           break;
         case 'kelvin':
-          result = `${temperature}K is ${(temperature - 273.15).toFixed(2)}°C`;
+          convertedTemperature = (temperature - 273.15);
+          result = `${temperature}K is ${convertedTemperature.toFixed(2)}°C`;
           break;
         default:
           result = 'Please select a valid unit!';
           break;
+      }
+  
+      // Update emoji based on the converted temperature
+      if (convertedTemperature < 0) {
+        emojiElement.textContent = '❄️'; // Cold
+      } else if (convertedTemperature > 30) {
+        emojiElement.textContent = '🔥'; // Hot
+      } else {
+        emojiElement.textContent = '😊'; // Normal
+      }
+  
+      // Adjusting background based on the converted temperature
+      if (convertedTemperature < 0) {
+        body.style.backgroundImage = 'url("https://source.unsplash.com/1600x900/?winter")';
+      } else if (convertedTemperature > 30) {
+        body.style.backgroundImage = 'url("https://source.unsplash.com/1600x900/?summer")';
+      } else {
+        body.style.backgroundImage = 'url("https://source.unsplash.com/1600x900/?spring")';
       }
     }
     resultElement.textContent = result;
